8b6b7a914d854ab1c885e917b643e7710d67d9ac
hjohannessen <hjohannessen@gmail.com>
2025-03-04 18:59:25 +0100

    Added TerraformDriftDetection action

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,9 +9,8 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "TEST_DEV"
     storage_account_name = "terraformcodeacademy"
-    container_name       = "TODO"
-    key                  = "terraform.TODO"
-    use_oidc             = true
+    container_name       = "tfstate32"
+    key                  = "terraform.tfstate32"
     subscription_id      = "e25e96db-c54e-4251-bed2-5d7a3e52fc4e"
     tenant_id            = "8b87af7d-8647-4dc7-8df4-5f69a2011bb5"
   }
@@ -20,5 +19,3 @@ provider "azurerm" {
   features {}
   use_oidc = true
 }
-
-data "azurerm_client_config" "current" {}
